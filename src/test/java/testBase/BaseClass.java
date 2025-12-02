81bd32ec2c4b48dea602e3d32338947f32ad237d
AlokQA <alokwrk@gmail.com>
2025-12-02 15:39:45 +0530

    uploading TestCase Excel file

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,22 +3,31 @@ package testBase;
 import java.time.Duration;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import net.bytebuddy.asm.Advice.This;
+
 public class BaseClass {
+	
 	public WebDriver driver;
+	public Logger logger;
+	
 	@BeforeClass
-	void setup() {
+	public void setup() {
+		System.out.println("Starting point");
+		logger = LogManager.getLogger(this.getClass());
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("http://localhost/opencart/");
 		driver.manage().window().maximize();
 	}
 	@AfterClass
-	void tearDown() {
+	public void tearDown() {
 		driver.quit();
 	}
 	
--- src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -1,6 +1,7 @@
 package testCases;
 
-import org.testng.Assert;
+import org.testng.Assert; 
+
 import org.testng.annotations.Test;
 
 import pageObjects.AccountRegPage;
@@ -12,12 +13,16 @@ public class TC001_AccountRegistrationTest extends BaseClass{
 	
 	@Test
 	public void verify_account_registration() {
+		logger.info("Starting the acc_reg method");
+		try {
 		HomePage hp = new HomePage(driver);
 		hp.clickMyAccount();
+		logger.info("Clicked on My Account");
 		hp.clickRegister();
+		logger.info("Clicked on Register");
 		
 		AccountRegPage arp = new AccountRegPage(driver);
-		
+		logger.info("Filling details");
 		arp.setFirstName(randomString().toUpperCase());
 		arp.setLastName(randomString().toUpperCase());
 		arp.setEmail(randomString()+"@yopmail.com");
@@ -27,7 +32,14 @@ public class TC001_AccountRegistrationTest extends BaseClass{
 		arp.continuebtn();
 		
 		String confirmMsg = arp.getConfirmationMsg();
+		logger.info("Validating expected message");
 		Assert.assertEquals(confirmMsg, "Your Account Has Been Created!");
+		}catch(Exception e) {
+			logger.info("Error");
+			logger.debug("Debug");
+			Assert.fail();
+		}
+		logger.info("Test finished");
 	
 	}
 	
