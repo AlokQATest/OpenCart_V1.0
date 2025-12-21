094e0e065f9b33613fb0d16613361f4cbb97eb1f
AlokQA <alokwrk@gmail.com>
2025-12-21 13:45:48 +0530

    Updated extent reprot

--- src/test/java/utilities/ExtentReportManager.java
+++ src/test/java/utilities/ExtentReportManager.java
@@ -1,5 +1,10 @@
 package utilities;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -15,41 +20,56 @@ public class ExtentReportManager implements ITestListener{
 	public ExtentSparkReporter Sparkreporter; // UI of the report
 	public ExtentReports report; // Common info
 	public ExtentTest test;  // For creating test entries
-	
+		   String repName;
 	 public void onStart(ITestContext context) {
-		 Sparkreporter = new ExtentSparkReporter("./reports/MyExtReport.html");  
 		 
-		 Sparkreporter.config().setDocumentTitle("Automation Testing");
-		 Sparkreporter.config().setReportName("Functional Testing");
+		 String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+		 repName = "Test-Report"+ timeStamp+".html";
+		 Sparkreporter = new ExtentSparkReporter("./reports"+ repName);  
+		 
+		 Sparkreporter.config().setDocumentTitle("Opencart Automation Testing Report");
+		 Sparkreporter.config().setReportName("Opencart Functional Testing");
 		 Sparkreporter.config().setTheme(Theme.DARK);
 		 
 		 report = new ExtentReports();
 		 report.attachReporter(Sparkreporter);
 		 
-		 report.setSystemInfo("Computer Name", "Localhost");
+		 report.setSystemInfo("Application", "Opencart");
+		 report.setSystemInfo("Module","Admin");
+		 report.setSystemInfo("SubModule", "customer");
+		 report.setSystemInfo("Username", System.getProperty("user.name"));
 		 report.setSystemInfo("Environment", "QA");
-		 report.setSystemInfo("Operating System", "MacOS");
+		 String os = context.getCurrentXmlTest().getParameter("os");
+		 report.setSystemInfo("Operating System", os);
+		 String browser = context.getCurrentXmlTest().getParameter("browser");
+		 report.setSystemInfo("Browser Name", browser);
 		 report.setSystemInfo("Tester Name", "Alok Tiwari");
-		 report.setSystemInfo("Browser Name", "Chrome");
 		 
+		 List<String> IncludedGroups = context.getCurrentXmlTest().getIncludedGroups();
+		 if(!IncludedGroups.isEmpty()) {
+			 report.setSystemInfo("Groups", IncludedGroups.toString());
+		 }
 	  }
 	 
 	 
 	 public void onTestSuccess(ITestResult result) {
-		 test = report.createTest(result.getName()); // create new entry in report
-		 test.log(Status.PASS, "Testcase passed: " + result.getName()); //update status Pass/Fail/Skip
+		 test = report.createTest(result.getTestClass().getName()); // create new entry in report
+		 test.assignCategory(result.getMethod().getGroups()); // to display groups in report
+		 test.log(Status.PASS, result.getName()+ " Testcase got successfully executed"); //update status Pass/Fail/Skip
 	  }
 
 	  
 	 public void onTestFailure(ITestResult result) {
 	    test = report.createTest(result.getName());
-	    test.log(Status.FAIL, "Testcase failed: " + result.getName());
+	    test.assignCategory(result.getMethod().getGroups());
+	    test.log(Status.FAIL, result.getName() + " Testcase got failed" );
 	  }
 
 	  
 	  public void onTestSkipped(ITestResult result) {
 	    test = report.createTest(result.getName());
-	    test.log(Status.SKIP, "Testcase skipped: "+ result.getName());
+	    test.assignCategory(result.getMethod().getGroups());
+	    test.log(Status.SKIP, result.getName()+ " Testcase got skipped");
 	  }
 	  
 	 
